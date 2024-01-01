b2195a59dda4f91f6922ee8e88e08437d5d57db5
Tokuhiro Matsuno <tokuhirom@linecorp.com>
2024-01-02 00:19:44 +0900

    remove matches_hotkey_string

--- src-tauri/src/js_builtin.rs
+++ src-tauri/src/js_builtin.rs
@@ -1,8 +1,6 @@
 use apple_sys::CoreGraphics::{CGEventFlags, CGKeyCode};
 use boa_engine::{Context, JsArgs, JsError, JsNativeError, JsResult, JsString, JsValue};
-use boa_gc::{GcRefCell};
 use crate::app_config::AppConfig;
-use crate::hotkey::HotKey;
 use crate::send::{send_flags_changed_event, send_keyboard_event};
 
 pub struct JsBuiltin {
@@ -10,28 +8,6 @@ pub struct JsBuiltin {
 }
 
 impl JsBuiltin {
-    pub fn matches_hotkey_string(_this: &JsValue, args: &[JsValue], context: &mut Context<'_>) -> JsResult<JsValue> {
-        let flags: &JsValue = args.get_or_undefined(0);
-        let keycode = args.get_or_undefined(1);
-        let shortcut = args.get_or_undefined(2);
-        let shortcut = shortcut.as_string().unwrap().to_std_string().unwrap();
-
-        match HotKey::from_str(shortcut.as_str()) { // TODO cache? config をパースしたタイミングで、ショートカットのパースもしておくべき
-            Ok(hotkey) => {
-                let result = hotkey.matches(
-                    flags.to_i32(context).unwrap() as CGEventFlags,
-                    keycode.to_i32(context).unwrap() as CGKeyCode);
-
-                Ok(JsValue::from(result))
-            }
-            Err(err) => {
-                Err(JsNativeError::typ()
-                    .with_message(format!("Cannot run parse_shortcut: {:?}", err))
-                    .into())
-            }
-        }
-    }
-
     pub fn send_flags_changed_event(_this: &JsValue, args: &[JsValue], context: &mut Context<'_>) -> JsResult<JsValue> {
         let flags: &JsValue = args.get_or_undefined(0);
 
