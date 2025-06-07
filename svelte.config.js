8bebe51c5879f34158206231ee7ee87b6cd6742a
Tokuhiro Matsuno <tokuhirom@gmail.com>
2025-06-08 07:37:26 +0900

    Enable Svelte 5's backward compatibility mode

--- svelte.config.js
+++ svelte.config.js
@@ -4,4 +4,12 @@ export default {
     // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
     // for more information about preprocessors
     preprocess: vitePreprocess(),
+
+    // Enable Svelte 5's backward compatibility mode
+    // https://svelte.dev/docs/svelte/v5-migration-guide#Components-are-no-longer-classes
+    compilerOptions: {
+        compatibility: {
+            componentApi: 4
+        }
+    }
 };
